11f01f041c332f1a1cdd1fff6ab9a442dd05438f
rutuj mirzapure <44692465+rutujmirzapure@users.noreply.github.com>
2019-12-19 09:35:48 +0530

    Add files via upload
    array of month using arraylist

---
+++ arrayMonths.java
@@ -0,0 +1,19 @@
+package collegeAssignment1;
+
+public class arrayMonths {
+	
+	
+	int months;
+	int days;
+	int date;
+	
+	
+	public static void main(String[] args) {
+		int month_days[]= {31 ,28 ,31,30,31,30,31,31,30,31,30,31};
+		
+		System.out.println(("april has")+month_days[3]+"days");
+				
+		}
+	}
+
+
